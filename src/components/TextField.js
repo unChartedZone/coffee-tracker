64b3e725372d14e114b0efa8610c6e6c8bc508da
Christian Valdez <christianvaldez36@hotmail.com>
2020-11-12 16:23:45 -0800

    created textfield component

---
+++ src/components/TextField.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const TextFieldStyles = styled.div`
+  font-size: 2rem;
+
+  input {
+    padding: 1rem;
+    border: 1px solid #aaa;
+    border-radius: 7px;
+  }
+`;
+
+const TextField = ({ type, placeholder, value, onChange }) => {
+  return (
+    <TextFieldStyles>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </TextFieldStyles>
+  );
+};
+
+export default TextField;
