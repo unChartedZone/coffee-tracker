1d3d01eeda6447cdef1fa935d18b246b9c1f38ef
Christian Valdez <christianvaldez36@hotmail.com>
2020-11-17 21:19:56 -0800

    styled places into a card like component

--- src/components/CoffeePlaces.js
+++ src/components/CoffeePlaces.js
@@ -4,24 +4,46 @@ import styled from 'styled-components';
 
 const PlacesStyled = styled.div`
   ul {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
     list-style: none;
 
     & > li {
       margin-bottom: 2rem;
     }
   }
+`;
 
-  img {
-    width: 100px;
-    height: 100px;
-  }
+const Place = styled.li`
+  font-size: 1.6rem;
+  border-radius: 7px;
+  background-color: white;
+  color: black;
+  height: 15rem;
+  width: 40rem;
+  display: grid;
+  grid-template-columns: 30% 40%;
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+    0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  overflow: hidden;
 
-  li {
-    font-size: 1.6rem;
-    border-radius: 7px;
-    background-color: white;
-    color: black;
-    padding: 1rem;
+  .place {
+    &__img {
+      overflow: hidden;
+      background-color: purple;
+
+      img {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+      }
+    }
+
+    &__info {
+      padding: 0.4rem;
+    }
   }
 `;
 
@@ -35,10 +57,15 @@ const CoffeePlaces = () => {
         <ul>
           {places.map((place) => {
             return (
-              <li key={place.id}>
-                <img src={place.image_url} alt={place.name} />
-                <h1 className="title">{place.name}</h1>
-              </li>
+              <Place key={place.id}>
+                <div className="place__img">
+                  <img src={place.image_url} alt={place.name} />
+                </div>
+                <div className="place__info">
+                  <h1 className="place__title title">{place.name}</h1>
+                  <p>{place.name}</p>
+                </div>
+              </Place>
             );
           })}
         </ul>
